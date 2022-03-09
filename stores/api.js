f60d9f49a353e28e73e9266a9aaa192729563b0a
Statcomm <63654894+Statcomm@users.noreply.github.com>
2022-03-09 15:32:12 +0300

    Beneficiaries only lacking update

--- stores/api.js
+++ stores/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-export const baseURL = "http://localhost:8000";
+export const baseURL = "http://192.168.150.79:8000";
 
 const api = axios.create({ baseURL: `${baseURL}` });
 
